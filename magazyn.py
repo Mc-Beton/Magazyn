6d5cd915c7b5da09d35a7e60ffdea1547d66b3e2
Filip Gabrys <el.filberto@gmail.com>
2020-05-06 16:36:12 +0200

    Add function sell

--- magazyn.py
+++ magazyn.py
@@ -1,9 +1,9 @@
 #items in warehouse
 
 
-Product1 = ['Bohus123', 10, 'm2', 1050]
-Product2 = ['Bohus1234', 8, 'm2', 1700]
-Product3 = ['Bohus12', 12, 'm2', 1400]
+Product1 = ['Bohus Grey', 10, 'm2', 1050]
+Product2 = ['Jet Black', 8, 'szt', 1700]
+Product3 = ['Viscont White', 12, 'm2', 1400]
 
 Products = []
 Products.append(Product1)
@@ -19,24 +19,44 @@ def items_get(items):
                 print(f"{name}\t\t{amount}\t\t{unit}\t{price}")
             else:
                 print(f"{name}\t{amount}\t\t{unit}\t{price}")
+    task()
 
 def items_add():
-    Name = input("Podaj nazwę produktu")
-    Amount = input("Podaj ilość produktu")
-    Unit = input("Podaj jednostkę miary produktu")
-    Price = input(f"Podaj cenę za {Unit} produktu")
+    Name = input("Podaj nazwę produktu ")
+    Amount = input("Podaj ilość produktu ")
+    Unit = input("Podaj jednostkę miary produktu ")
+    Price = input(f"Podaj cenę za {Unit} produktu ")
     new_product = [Name, Amount, Unit, Price]
     Products.append(new_product)
-    items_get(Products)
-            
-            
-        
+    task()
 
-task = input("What would you like to do?")
-if task == 'exit':
-    print("exiting program")
-if task == 'show':
-    items_get(Products)
-if task == 'add':
-    items_add()
+def items_sell(items):
+    NameS = input("Co chcesz sprzedać? ")
+    for product in items:
+        if product[0]==NameS:
+            UnitS=product[2]
+            AmountS = int(input(f"Ile {UnitS} {NameS} chceesz sprzedać?"))
+            product[1]=product[1]-AmountS
+            print(f"Sprzedano {AmountS} {UnitS} {NameS}")           
+    
+    task()
+
+                 
+def task():
+    task = input("What would you like to do?")
+    if task == 'exit':
+        print("exiting program")
+    if task == 'show':
+        items_get(Products)
+    if task == 'add':
+        items_add()
+    if task == 'sell':
+        items_sell(Products)
+
+#wywołanie programu
+if __name__ == "__main__":
+    task()
+    
+
+
 
