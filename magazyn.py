91d2bfa3eeb65211b06c436bc65601c01cec5974
Filip Gabrys <el.filberto@gmail.com>
2020-05-06 16:00:43 +0200

    Add function add new product, also repair print in show function

--- magazyn.py
+++ magazyn.py
@@ -1,9 +1,9 @@
 #items in warehouse
 
 
-Product1 = ['Bohus_Grey', 10, 'm2', 1050]
-Product2 = ['Jet Black', 8, 'm2', 1700]
-Product3 = ['Viscont_White', 12, 'm2', 1400]
+Product1 = ['Bohus123', 10, 'm2', 1050]
+Product2 = ['Bohus1234', 8, 'm2', 1700]
+Product3 = ['Bohus12', 12, 'm2', 1400]
 
 Products = []
 Products.append(Product1)
@@ -15,8 +15,19 @@ def items_get(items):
     print("Name\t\tQuantity\tUnit\tUnit Price (PLN)")
     for product in items:
         for name, amount, unit, price in list(zip(*([iter(product)] * 4))):
-            print(f"{name}\t{amount}\t\t{unit}\t{price}")
+            if len(name) < 8:
+                print(f"{name}\t\t{amount}\t\t{unit}\t{price}")
+            else:
+                print(f"{name}\t{amount}\t\t{unit}\t{price}")
 
+def items_add():
+    Name = input("Podaj nazwę produktu")
+    Amount = input("Podaj ilość produktu")
+    Unit = input("Podaj jednostkę miary produktu")
+    Price = input(f"Podaj cenę za {Unit} produktu")
+    new_product = [Name, Amount, Unit, Price]
+    Products.append(new_product)
+    items_get(Products)
             
             
         
@@ -26,5 +37,6 @@ if task == 'exit':
     print("exiting program")
 if task == 'show':
     items_get(Products)
-
+if task == 'add':
+    items_add()
 
