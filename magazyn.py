2a3bde08d7ce52484e1bc69556de6a4f2442c493
Filip Gabrys <el.filberto@gmail.com>
2020-05-18 17:06:02 +0200

    asd

--- magazyn.py
+++ magazyn.py
@@ -1,5 +1,9 @@
-#items in warehouse
+import csv
+from decimal import *
+getcontext().prec = 2
+global Products
 
+#items in warehouse
 
 Product1 = ['Bohus Grey', 10, 'm2', 1050]
 Product2 = ['Jet Black', 8, 'szt', 1700]
@@ -24,9 +28,9 @@ def items_get(items):
 
 def items_add():
     Name = input("Podaj nazwę produktu ")
-    Amount = input("Podaj ilość produktu ")
+    Amount = float(input("Podaj ilość produktu "))
     Unit = input("Podaj jednostkę miary produktu ")
-    Price = input(f"Podaj cenę za {Unit} produktu ")
+    Price = float(input(f"Podaj cenę za {Unit} produktu "))
     new_product = [Name, Amount, Unit, Price]
     Products.append(new_product)
     task()
@@ -37,7 +41,7 @@ def items_sell(items):
         if product[0]==NameS:
             PriceS=product[3]
             UnitS=product[2]
-            AmountS = int(input(f"Ile {UnitS} {NameS} chceesz sprzedać?"))
+            AmountS = float(input(f"Ile {UnitS} {NameS} chceesz sprzedać?"))
             product[1]=product[1]-AmountS
             print(f"Sprzedano {AmountS} {UnitS} {NameS}")
             Sold = [NameS, AmountS, UnitS, PriceS]
@@ -50,13 +54,60 @@ def get_costs(items, sold):
     for product in items:
         cost1 = product[1]*product[3]
         costs = costs + cost1
+        Decimal(costs)
     print(f"Costs: {costs}")
     for product in sold:
         income1= product[1]*product[3]
         income=income +income1
+        Decimal(income)
     print(f"Income: {income}")
     print("_____________")
     print(f"Revenue: {income-costs}")
+    task()
+
+def export_items_to_csv(items):
+    dict_pro={}
+    for i in Products:
+        dict_pro[i[0]]={}
+    for i in Products:
+        dict_pro[i[0]]={'Name':i[0], 'Amount':i[1], 'Unit': i[2], 'Price': i[3]}
+
+    with open('magazyn.csv', 'w', newline='') as csvfile:
+        fieldnames = ['pro_name', 'pro_info']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for i,j in dict_pro.items():
+            writer.writerow({'pro_name': i, 'pro_info': j})
+
+def export_sells_to_csv(items):
+    dict_pro={}
+    for i in items:
+        dict_pro[i[0]]={}
+    for i in items:
+        dict_pro[i[0]]={i[1], i[2], i[3]}
+
+    with open('sold_magazyn.csv', 'w', newline='') as csvfile:
+        fieldnames = ['pro_name', 'pro_info']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for i,j in dict_pro.items():
+            writer.writerow({i, j})
+    task()
+
+def load_items_from_csv():
+    a=[]
+    dict_pro={}
+    with open('magazyn2.csv', newline='') as csvfile:
+        Products1 = csv.DictReader(csvfile)
+        for row in Products1:
+            for i, j in row.items():
+                a.append(j)
+            for k, l in list(zip(*([iter(a)] * 2))):
+                dict_pro[k]=l
+    for i in dict_pro.values():
+        for key, value in i.items():
+            a.append(value)
+    for name, amount, unit, price in list(zip(*([iter(a)] * 4))):
+        Products=[name, amount, unit, price]
+    task()
                  
 def task():
     task = input("What would you like to do?")
@@ -70,6 +121,12 @@ def task():
         items_sell(Products)
     if task == 'show_revenue':
         get_costs(Products, Sold_Products)
+    if task == 'save':
+        export_items_to_csv(Products)
+        export_sells_to_csv(Sold_Products)
+    if task == 'load':
+        Products.clear()
+        load_items_from_csv()
 
 #wywołanie programu
 if __name__ == "__main__":
